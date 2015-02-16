ee7ed3d487a519565d7be23db60e45d87280d7b5
unknown <aa797@cdpc42.medschl.cam.ac.uk>
2015-02-16 17:42:20 +0000

    -- Tested out popups for GeoJSON by naming one of the features as 'popup' in the geojson file. Now reverting the change.

--- fixMyPath/ui.R
+++ fixMyPath/ui.R
@@ -9,7 +9,7 @@ fluidPage(
       ,  sliderInput("transp_zones", label = "Transparency of zone boundaries", min = 0, max = 1, value = 0)
       , sliderInput("transp_fast", label = "Transparency of paths", min = 0, max = 1, value = 0)
       , selectInput("viewout", "Output to view", choices = c("Highest cycle counts", "Lowest number who cycle", "Highest potential", "Greatest extra cycling potential"))
-      , selectInput("feature", "Features", choices = c("none", "cycleparking", "collisions", "bikeshopsp"))
+      , selectInput("feature", "Features", choices = c("none", "cycleparking", "collisions", "bikeshops"))
       , htmlWidgetOutput(
         outputId = 'desc',
         HTML(paste(
