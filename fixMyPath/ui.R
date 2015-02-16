594de91a71f282da8cfce20ed5971ccf7ccbbf91
unknown <aa797@cdpc42.medschl.cam.ac.uk>
2015-02-16 17:38:32 +0000

    -- Cleaner code

--- fixMyPath/ui.R
+++ fixMyPath/ui.R
@@ -6,15 +6,10 @@ fluidPage(
   sidebarLayout(
     sidebarPanel("User input", width = 3 , 
        checkboxInput("layers", "Show Layers", TRUE)
-      #, conditionalPanel(
-      #   condition = "input.layers == true"
-      #   , sliderInput("transp_zones", label = "Transparency of zone boundaries", min = 0, max = 1, value = 0)
-      #   , sliderInput("transp_fast", label = "Transparency of paths", min = 0, max = 1, value = 0)
-      # )
       ,  sliderInput("transp_zones", label = "Transparency of zone boundaries", min = 0, max = 1, value = 0)
       , sliderInput("transp_fast", label = "Transparency of paths", min = 0, max = 1, value = 0)
       , selectInput("viewout", "Output to view", choices = c("Highest cycle counts", "Lowest number who cycle", "Highest potential", "Greatest extra cycling potential"))
-      , selectInput("feature", "Features", choices = c("none", "cycleparking", "collisions", "bikeshops"))
+      , selectInput("feature", "Features", choices = c("none", "cycleparking", "collisions", "bikeshopsp"))
       , htmlWidgetOutput(
         outputId = 'desc',
         HTML(paste(
@@ -25,12 +20,5 @@ fluidPage(
     ),
     mainPanel(
      leafletOutput('map', height = 600)
-    
-    
-    #leafletMap(
-    #  "map", "100%", 400,
-    #  initialTileLayer = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
-    #  initialTileLayerAttribution = HTML('Maps by <a href="http://www.mapbox.com/">Mapbox</a>')
-    #)
     )
   ))
