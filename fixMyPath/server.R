351086916fa111d96dc431fdf352b61652c4b4d1
unknown <aa797@cdpc42.medschl.cam.ac.uk>
2015-02-16 19:08:37 +0000

    -- Data for polygons and polylines is supplied ONLY when the show layers checkbox is set to true.

--- fixMyPath/server.R
+++ fixMyPath/server.R
@@ -8,7 +8,7 @@ library(reshape)
 
 # Load data
 map_centre <- geocode("Leeds")
-pedallers <- geocode("Mabgate Green")
+#pedallers <- geocode("Mabgate Green")
 l <- readRDS("al.Rds")
 
 l$color <- "green"
@@ -33,49 +33,69 @@ shinyServer(function(input, output, session){
 cents <- coordinates(leeds)
 cents <- SpatialPointsDataFrame(cents, data = leeds@data, match.ID = F)
 observe({
+   
+   geojson <- RJSONIO::fromJSON(sprintf("%s.geojson", input$feature))
+   
+   # Create a leaflet instance without any transport layers (fast or slow lanes)
+   m <- leaflet() %>%
+     addTiles(urlTemplate = "http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png")  %>%
+     addCircleMarkers(data = cents
+                      , radius = 2
+                      , color = "black"
+                      , popup = sprintf("<b>Journeys by bike: </b>%s%%", round(ldata$pCycle*100,2))) %>%
+     addGeoJSON(geojson) %>%
+     #addPopups(pedallers$lon, pedallers$lat, "The best bike shop in Leeds!") %>%
+     mapOptions(zoomToLimits = "first") 
+     #%>%
+     #addPolylines(data = lfast, color = "red"
+    #              , opacity = input$transp_fast
+    #              , popup = sprintf("<dl><dt>Distance </dt><dd>%s km</dd><dt>Journeys by bike</dt><dd>%s%%</dd>", round(flows$fastest_distance_in_m / 1000, 1), round(flows$p_cycle * 100, 2))
+    # ) %>%
+    # addPolylines(data = lquiet, color = "green",
+    #              , opacity = input$transp_fast, 
+    #              popup = sprintf("<dl><dt>Distance </dt><dd>%s km</dd><dt>Journeys by bike</dt><dd>%s%%</dd>", round(flows$quietest_distance_in_m / 1000, 1), round(flows$p_cycle*100,2))
+    # )
+   
    if (input$layers == FALSE){
      updateSliderInput(session, "transp_zones", value = 0, min = 0, max = 1)       
-     updateSliderInput(session, "transp_fast", value = 0, min = 0, max = 1)       
+     updateSliderInput(session, "transp_fast", value = 0, min = 0, max = 1)  
+     
    }else {
+     
      updateSliderInput(session, "transp_zones", value = input$transp_zones, min = 0, max = 1)       
-     updateSliderInput(session, "transp_fast", value = input$transp_fast, min = 0, max = 1)       
+     updateSliderInput(session, "transp_fast", value = input$transp_fast, min = 0, max = 1)
      
+     ## Only add polygones (of MSOA boundaries) and also of route when 'show layers' is set to TRUE
+     m <- m %>%
+       addPolygons(data = leeds
+                   , fillOpacity = 0.4
+                   , opacity = input$transp_zones
+                   , fillColor = leeds$color_pcycle
+                   
+       ) %>%
+       addPolylines(data = lfast, color = "red"
+                    , opacity = input$transp_fast
+                    , popup = sprintf("<dl><dt>Distance </dt><dd>%s km</dd><dt>Journeys by bike</dt><dd>%s%%</dd>", round(flows$fastest_distance_in_m / 1000, 1), round(flows$p_cycle * 100, 2))
+       ) %>%
+       addPolylines(data = lquiet, color = "green",
+                    , opacity = input$transp_fast, 
+                    popup = sprintf("<dl><dt>Distance </dt><dd>%s km</dd><dt>Journeys by bike</dt><dd>%s%%</dd>", round(flows$quietest_distance_in_m / 1000, 1), round(flows$p_cycle*100,2))
+      )
    }
-   geojson <- RJSONIO::fromJSON(sprintf("%s.geojson", input$feature))
+ 
+    output$map = renderLeaflet(m)
 
-   output$map = renderLeaflet(
-     leaflet() %>%
-     addTiles(urlTemplate = "http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png") %>%
-     addPolygons(data = leeds
-       , fillOpacity = 0.4
-       , opacity = input$transp_zones
-       , fillColor = leeds$color_pcycle
-     
-     ) %>%
-     addPolylines(data = lfast, color = "red"
-                  , opacity = input$transp_fast
-       , popup = sprintf("<dl><dt>Distance </dt><dd>%s km</dd><dt>Journeys by bike</dt><dd>%s%%</dd>", round(flows$fastest_distance_in_m / 1000, 1), round(flows$p_cycle * 100, 2))
-                          ) %>%
-     addPolylines(data = lquiet, color = "green",
-                  , opacity = input$transp_fast, popup = sprintf("<dl><dt>Distance </dt><dd>%s km</dd><dt>Journeys by bike</dt><dd>%s%%</dd>", round(flows$quietest_distance_in_m / 1000, 1), round(flows$p_cycle*100,2))
-                  ) %>%
-     addCircleMarkers(data = cents
-                , radius = 2
-                , color = "black"
-                , popup = sprintf("<b>Journeys by bike: </b>%s%%", round(ldata$pCycle*100,2))) %>%
-     addGeoJSON(geojson) %>%
-     addPopups(pedallers$lon, pedallers$lat, "The best bike shop in Leeds!") %>%
-     mapOptions(zoomToLimits = "first")
-  )
- })
+})
+
 output$desc <- reactive({
   if (is.null(input$map_bounds))
     return(list())
   list(
-    lat = mean(c(input$map_bounds$north, input$map_bounds$south)),
-    lng = mean(c(input$map_bounds$east, input$map_bounds$west)),
+    lat = round(mean(c(input$map_bounds$north, input$map_bounds$south)), 1),
+    lng = round(mean(c(input$map_bounds$east, input$map_bounds$west)), 1),
     zoom = input$map_zoom
-    )
-  })
+  )
 })
 
+
+})
--- fixMyPath/ui.R
+++ fixMyPath/ui.R
@@ -4,9 +4,9 @@ library(leaflet)
 fluidPage(
   titlePanel("FixMyPath"),
   sidebarLayout(
-    sidebarPanel("User input", width = 3 , 
-       checkboxInput("layers", "Show Layers", TRUE)
-      ,  sliderInput("transp_zones", label = "Transparency of zone boundaries", min = 0, max = 1, value = 0)
+    sidebarPanel("User input", width = 3
+      , checkboxInput("layers", "Show Layers", TRUE)
+      , sliderInput("transp_zones", label = "Transparency of zone boundaries", min = 0, max = 1, value = 0)
       , sliderInput("transp_fast", label = "Transparency of paths", min = 0, max = 1, value = 0)
       , selectInput("viewout", "Output to view", choices = c("Highest cycle counts", "Lowest number who cycle", "Highest potential", "Greatest extra cycling potential"))
       , selectInput("feature", "Features", choices = c("none", "cycleparking", "collisions", "bikeshops"))
